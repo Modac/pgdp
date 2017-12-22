aa86b558a0f87a49bc6242a4bf239ee900fd307f
BuildTools <unconfigured@null.spigotmc.org>
2017-12-22 16:56:23 +0100

    Blatt8 Aufgabe 8.5 Comments

--- src/main/java/Blatt8/Worstorage.java
+++ src/main/java/Blatt8/Worstorage.java
@@ -21,22 +21,33 @@ public class Worstorage {
   }
 
   public void add(Penguin penguin) {
+
+
     int p = 1;
     for (int i = 0; i <= count.length; i++) {
       //System.out.println("p: " + p);
+
+      // Wenn eine neue Ebene hinzugefügt werden muss
       if (p > store.length) {
         expandStorage();
       }
+
 
       if (store[p - 1] == null) {
         store[p - 1] = penguin;
         count[i]++;
       }
+
+      // Wenn schon vorhanden
       if (penguin.compareTo(store[p - 1]) == 0) {
         return;
-      } else if (penguin.compareTo(store[p - 1]) > 0) {
+      }
+      // Wenn der neue Pinguin knuddeliger ist, gehe in den rechten Teilbaum
+      else if (penguin.compareTo(store[p - 1]) > 0) {
         p = 2 * p + 1;
-      } else {
+      }
+      // Sonst gehe in den linken
+      else {
         p = 2 * p;
       }
     }
@@ -50,12 +61,13 @@ public class Worstorage {
 
   public boolean find(Penguin penguin) {
     int p = 1;
-    for (int i = 0; i < count.length; i++) {
+    for (int aCount : count) {
 
       if (store[p - 1] == null || p > store.length) {
         return false;
       }
 
+      // Fast genau so wie in add()
       if (penguin == store[p - 1]) {
         return true;
       } else if (penguin.compareTo(store[p - 1]) > 0) {
@@ -69,7 +81,7 @@ public class Worstorage {
 
   public void remove(Penguin penguin) {
     int p = 1;
-    for (int i = 0; i < count.length; i++) {
+    for (int aCount : count) {
 
       if (store[p - 1] == null || p > store.length) {
         return;
@@ -92,12 +104,13 @@ public class Worstorage {
     int p = position;
 
     // Wenn das zu löschende Element keine Nachfolger hat
-    // dann lösche es einfach
+    // dann setzte es einfach gleich null
     if ((2*p > store.length || store[2 * p - 1] == null) && (2*p+1 > store.length || store[2 * p] == null)) {
       store[p - 1] = null;
     }
 
     // Wenn es einen Nachfolger hat
+    // dann verschiebe den nachfolgenden Teilbaum an die Stelle von dem Element
     else if (store[2 * p - 1] != null ^ store[2 * p] != null) {
       int n = 2 * p;
       if (store[2 * p] != null) {
@@ -107,6 +120,11 @@ public class Worstorage {
     }
 
     // Wenn es zwei Nachfolger hat
+    // dann suche nach dem größten Element im linken Teilbaum,
+    // ersetzte das zu löschende Element damit und lösche es anschließend selbst
+    //
+    // (Da es maximal einen weiteren Nachfolger hat, da es sonst nicht das größte Element wäre,
+    // ist das kein weiteres Problem)
     else {
 
       int n = 2*p;
@@ -134,8 +152,6 @@ public class Worstorage {
     }
 
     // System.out.println(Arrays.toString(count));
-
-    //count[getLevel(p)]--;
 
     updateCount();
 
@@ -172,16 +188,23 @@ public class Worstorage {
 
     // store[to - 1] = store[from - 1];
 
+    // Wird benötigt um später einen relativen Index im Teilbaum zu einem absoluten Index
+    // im Bezug auf den ganzen Baum umzurechnen
     int pDis = to - 1;
 
+    // Level
     int e = 0;
     for (to = 1; (e = getLevel(to)) < count.length; to++) {
 
+      // Das Element welches das an Index 'to' ersetzt liegt entweder am Index to + 2^e
+      // oder to + 2^(e+1)
       if (isLeft) {
         from = (int) (to + Math.pow(2, e));
       } else {
         from = (int) (to + Math.pow(2, e + 1));
       }
+
+      // Das ersetzende Element ist immer eine Ebene höher
       int fromI = subTreeIndexToArrayIndex(from, pDis, e + 1);
 
       int toI = subTreeIndexToArrayIndex(to, pDis, e);
@@ -195,20 +218,34 @@ public class Worstorage {
       System.out.println("Level: " + e);
       */
 
+      // Wenn fromI einmal ausserhalb des Arrays liegt wissen wir,
+      // dass alle darauf folgenden fromI's in mindestens der gleichen Ebene liegen werden.
+      // Und da diese Ebene nicht existiert können wir auch gleich aufhören
       if (fromI >= store.length) {
         return;
       }
-      if (store[fromI] != null) {
 
-        store[toI] = store[fromI];
-        store[fromI] = null;
-      }
+      store[toI] = store[fromI];
+      store[fromI] = null;
 
     }
 
   }
 
+  //         1
+  //      2    3
+  //   4   5 6   7
+  //
+  //   3    ->    1       3-1 = 2 = (3-1)*(2^0)
+  // 6   7  ->  2   3     6-2 = 4, 7-3 = 4  = (3-1)*(2^1)
+  //
+  //   2    ->    1       2-1 = 1 = (2-1)*(2^0)
+  // 4   5  ->  2   3     4-2 = 2, 5-3 = 2  = (2-1)*(2^1)
+  //
+  // allgemein:     Index innerhalb des gesamten Baums =
+  //    Index innerhalb des Teilbaums  + (Index der Wurzel des Teilbaums - 1) * (2 ^ Ebene des Index im Teilbaum)
   private int subTreeIndexToArrayIndex(int subTreeIndex, int subTreeRootIndex, int level) {
+
     return (subTreeIndex + subTreeRootIndex * (int) Math.pow(2, level)) - 1;
   }
 
@@ -226,10 +263,22 @@ public class Worstorage {
     return res;
   }
 
+  // Genau so wie toString() nur wird der Baum in In-Order traversiert und nicht in Level-Order
   public String toStringInorder() {
     return toStringInorder(1);
   }
 
+  private String toStringInorder(int i) {
+    String res = "";
+    res += (2 * i > store.length || store[2 * i - 1] == null) ? "" : (toStringInorder(2 * i));
+    res += store[i - 1] == null ? "" : (store[i - 1].getCuddliness() + ",");
+    res += (2 * i + 1 > store.length || store[2 * i] == null) ? "" : (toStringInorder(2 * i + 1));
+    return res;
+  }
+
+  // Ein sehr sporadischer versuch den Baum bäumisch darzustellen
+  // Klappt naja bei Werten mit nur einer Stelle
+  // Klappt noch schlechter bei großen Bäumen und mit Werten mit mehreren Stellen
   public void printTree() {
     int prevL = -1;
     for (int i = 0; i < store.length; i++) {
@@ -246,13 +295,6 @@ public class Worstorage {
     System.out.println("");
   }
 
-  private String toStringInorder(int i) {
-    String res = "";
-    res += (2 * i > store.length || store[2 * i - 1] == null) ? "" : (toStringInorder(2 * i));
-    res += store[i - 1] == null ? "" : (store[i - 1].getCuddliness() + ",");
-    res += (2 * i + 1 > store.length || store[2 * i] == null) ? "" : (toStringInorder(2 * i + 1));
-    return res;
-  }
 
   // Zum Testen (Code sollte außerhalb der zu testenden Klasse liegen)
   public static void main(String[] args) {
@@ -331,6 +373,8 @@ class Test {
     ws.add(pens[7]);
     System.out.println(ws.toString());
 
+    ws.printTree();
+
 
     /*
     Penguin[] storeT = new Penguin[((int) (Math.pow(2, 5)) - 1)];
@@ -357,13 +401,13 @@ class Test {
   }
 
   public static String stringNTimes(String str, int n) {
-    String res = "";
+    StringBuilder res = new StringBuilder();
 
     for (int i = 0; i < n; i++) {
-      res += str;
+      res.append(str);
     }
 
-    return res;
+    return res.toString();
   }
 
 }
